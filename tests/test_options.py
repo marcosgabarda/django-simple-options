490a59a5e1927b2a8d37ebb36350a80cc951bb27
Marcos Gabarda <hey@marcosgabarda.com>
2023-09-01 19:59:21 +0200

    feat: changes on tests

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+import pytest
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -7,39 +7,41 @@ from options.models import Option, UserOption
 from tests.factories import OptionFactory, UserFactory, UserOptionFactory
 
 
-class OptionTests(TestCase):
+@pytest.mark.django_db
+class OptionTests:
     def test_default_options(self):
         value = Option.objects.get_value("default_option", default="other")
-        self.assertEqual("default", value)
+        assert value == "default"
 
     def test_int_conversion_options(self):
         name = "int_option"
         OptionFactory(name=name, value="42", type=INT)
         value = Option.objects.get_value(name)
-        self.assertIsInstance(value, int)
-        self.assertEqual(42, value)
+        assert isinstance(value, int)
+        assert value == 42
 
     def test_str_conversion_options(self):
         name = "string_option"
         OptionFactory(name=name, value="42")
         value = Option.objects.get_value(name)
-        self.assertIsInstance(value, str)
-        self.assertEqual("42", value)
+        assert isinstance(value, str)
+        assert value == "42"
 
     def test_float_conversion_options(self):
         name = "float_option"
         OptionFactory(name=name, value="42.5", type=FLOAT)
         value = Option.objects.get_value(name)
-        self.assertIsInstance(value, float)
-        self.assertAlmostEqual(42.5, value)
+        assert isinstance(value, float)
+        assert value == 42.5
 
     def test_public(self):
         options = OptionFactory.create_batch(size=5, is_public=True)
         OptionFactory.create_batch(size=3, is_public=False)
-        self.assertEqual(len(options), Option.objects.public().count())
+        assert Option.objects.public().count() == len(options)
 
 
-class UserOptionTests(TestCase):
+@pytest.mark.django_db
+class UserOptionTests:
     def test_custom_user_options(self):
         user = UserFactory()
         name = "default_option"
@@ -48,7 +50,7 @@ class UserOptionTests(TestCase):
             name=name, public_name=name, value=expected_value, type=STR, user=user
         )
         value = UserOption.objects.get_value(name, user=user, default="other")
-        self.assertEqual(expected_value, value)
+        assert value == expected_value
 
 
 class OptionAPITests(APITestCase):
@@ -57,9 +59,9 @@ class OptionAPITests(APITestCase):
         admin = UserFactory(is_staff=True)
         self.client.force_authenticate(admin)
         response = self.client.get("/api/options/", format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert status.HTTP_200_OK == response.status_code
         data = response.json()
-        self.assertEqual(1 + len(options), len(data))
+        assert len(data) == 1 + len(options)
 
     def test_list_options_no_staff(self):
         OptionFactory.create_batch(size=10)
@@ -67,9 +69,9 @@ class OptionAPITests(APITestCase):
         admin = UserFactory()
         self.client.force_authenticate(admin)
         response = self.client.get("/api/options/", format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert status.HTTP_200_OK == response.status_code
         data = response.json()
-        self.assertEqual(len(options), len(data))
+        assert len(data) == len(options)
 
     def test_cant_update_options_no_staff(self):
         OptionFactory.create_batch(size=10)
@@ -80,7 +82,7 @@ class OptionAPITests(APITestCase):
         response = self.client.patch(
             f"/api/options/{options[0].pk}/", data=data, format="json"
         )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        assert status.HTTP_403_FORBIDDEN == response.status_code
 
     def test_can_update_options_staff(self):
         OptionFactory.create_batch(size=10)
@@ -91,7 +93,7 @@ class OptionAPITests(APITestCase):
         response = self.client.patch(
             f"/api/options/{options[0].pk}/", data=data, format="json"
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert status.HTTP_200_OK == response.status_code
 
     def test_list_user_options(self):
         user = UserFactory()
@@ -100,9 +102,9 @@ class OptionAPITests(APITestCase):
         UserOptionFactory(name=name, value=expected_value, user=user, is_public=True)
         self.client.force_authenticate(user)
         response = self.client.get("/api/user-options/", format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert status.HTTP_200_OK == response.status_code
         data = response.json()
-        self.assertEqual(1, len(data))
+        assert len(data) == 1
 
     def test_list_user_options_with_exclude(self):
         user = UserFactory()
@@ -114,9 +116,9 @@ class OptionAPITests(APITestCase):
         UserOptionFactory(name=name, value=expected_value, user=user, is_public=True)
         self.client.force_authenticate(user)
         response = self.client.get("/api/user-options/", format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert status.HTTP_200_OK == response.status_code
         data = response.json()
-        self.assertEqual(1, len(data))
+        assert len(data) == 1
 
     def test_create_user_options(self):
         user = UserFactory()
@@ -125,8 +127,8 @@ class OptionAPITests(APITestCase):
         data = {"name": name, "value": expected_value}
         self.client.force_authenticate(user)
         response = self.client.post("/api/user-options/", data=data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(1, UserOption.objects.filter(user=user).count())
+        assert status.HTTP_201_CREATED == response.status_code
+        assert UserOption.objects.filter(user=user).count() == 1
 
     def test_create_user_options_excluded(self):
         user = UserFactory()
@@ -135,4 +137,4 @@ class OptionAPITests(APITestCase):
         data = {"name": name, "value": expected_value}
         self.client.force_authenticate(user)
         response = self.client.post("/api/user-options/", data=data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        assert status.HTTP_400_BAD_REQUEST == response.status_code
